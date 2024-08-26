84e9e7d5b53711aa3828285251e1dad38ddef83a
Bunyamin Genc <bunyaming@uia.no>
2024-08-26 20:58:00 +0200

    navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,25 +8,27 @@ const Navbar = () => {
     <nav className='navbar mb-20 flex items-center justify-between py-6 px-4 md:px-8 font-bold '>
       <div className='flex items-center'>
         <img className="w-16 border-r-2 pr-4" src={logo} alt="logo" />
-        <h3 className='text-2xl md:text-3xl pl-4 font-bold hover:text-lime-300'>GRUPPE 7 </h3>
+        <h3 className='text-2xl text-center md:text-3xl pl-4 font-bold  hover:text-lime-300'>GRUPPE 7 </h3>
       </div>
 
       {/* Hamburger Menu */}
+      
+
+      {/* Navbar Links */}
+      <div className={`flex ${isOpen ? 'flex-col' : 'hidden'} mt-8 md:mt-0 w-full md:w-auto md:flex md:flex-row items-center justify-center gap-4 md:gap-8 text-xl`}>
+        <a href="#hjem" className="hover:text-lime-300">Hjem</a>
+        <a href="#om-oss" className="hover:text-lime-300">Om oss</a>
+        <a href="#medlemmer" className="hover:text-lime-300">Medlemmer</a>
+        <a href="#technologies" className="hover:text-lime-300">Technologies</a>
+        <a href="#projects" className="hover:text-lime-300">Projects</a>
+      </div> 
+
       <div className="md:hidden">
         <button onClick={() => setIsOpen(!isOpen)} className="text-white focus:outline-none ">
           <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
           </svg>
         </button>
-      </div>
-
-      {/* Navbar Links */}
-      <div className={`flex ${isOpen ? 'flex-col' : 'hidden'} md:flex md:flex-row items-center justify-center gap-4 md:gap-8 text-xl`}>
-        <a href="#hjem" className="hover:text-lime-300">Hjem</a>
-        <a href="#om-oss" className="hover:text-lime-300">Om oss</a>
-        <a href="#medlemmer" className="hover:text-lime-300">Medlemmer</a>
-        <a href="#technologies" className="hover:text-lime-300">Technologies</a>
-        <a href="#projects" className="hover:text-lime-300">Projects</a>
       </div>
     </nav>
   );
